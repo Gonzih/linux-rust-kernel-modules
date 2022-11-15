cf9e193312345685b1210a45bb6c56a3a2ffae22
Max Gonzih <gonzih@gmail.com>
2022-11-15 14:05:46 -0800

    Getting somewhere

--- samples/rust/rust_gonzih.rs
+++ samples/rust/rust_gonzih.rs
@@ -1,6 +1,9 @@
 //! Gonzih module
 
+use kernel::io_buffer::{IoBufferReader, IoBufferWriter};
 use kernel::prelude::*;
+use kernel::sync::{Arc, ArcBorrow, RwSemaphore};
+use kernel::{file, miscdev};
 
 module! {
     type: Gonzih,
@@ -10,7 +13,51 @@ module! {
     license: "GPL",
 }
 
-struct Gonzih {}
+struct Device {
+    number: usize,
+    contents: RwSemaphore<Vec<u8>>,
+}
+
+struct Gonzih {
+    _dev: Pin<Box<miscdev::Registration<Gonzih>>>,
+}
+
+#[vtable]
+impl file::Operations for Gonzih {
+    type OpenData = Arc<Device>;
+    type Data = Arc<Device>;
+
+    fn open(context: &Self::OpenData, _file: &file::File) -> Result<Self::Data> {
+        pr_info!("File for device {} was opened\n", context.number);
+        Ok(context.clone())
+    }
+
+    fn read(
+        data: ArcBorrow<'_, Device>,
+        _file: &file::File,
+        writer: &mut impl IoBufferWriter,
+        _offset: u64,
+    ) -> Result<usize> {
+        let buf = data.contents.read();
+        let n = buf.len();
+        writer.write_slice(&buf[..])?;
+        pr_info!("File was read {} {} bytes\n", data.number, n);
+        Ok(n)
+    }
+
+    fn write(
+        data: ArcBorrow<'_, Device>,
+        _file: &file::File,
+        reader: &mut impl IoBufferReader,
+        _offset: u64,
+    ) -> Result<usize> {
+        let n = reader.len();
+        let mut buf = data.contents.write();
+        reader.read_slice(&mut buf[..])?;
+        pr_info!("File was written {} {} bytes\n", data.number, n);
+        Ok(n)
+    }
+}
 
 impl kernel::Module for Gonzih {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
@@ -18,6 +65,13 @@ impl kernel::Module for Gonzih {
         pr_info!("Rust gonzih sample (init)\n");
         pr_info!("Am I built-in? {}\n", !cfg!(MODULE));
         pr_info!("====================================");
-        Ok(Gonzih {})
+
+        let dev = Arc::try_new(Device {
+            number: 0,
+            contents: unsafe { RwSemaphore::new(Vec::new()) },
+        })?;
+        let reg = miscdev::Registration::<Gonzih>::new_pinned(fmt!("gonzih"), dev)?;
+
+        Ok(Gonzih { _dev: reg })
     }
 }
